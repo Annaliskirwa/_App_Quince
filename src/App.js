be41f1622f40dd627736db0b3640ae5ce86f8392
Annaliskirwa <annaliskirwa@gmail.com>
2022-02-10 22:23:33 +0300

    installed react router, created homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 
+const HomePage = () => {
+  return <>Home Page</>;
+};
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Route exact path="/" component={HomePage} />
+      </Router>
+    </>
+  );
 };
 
 export default App;
