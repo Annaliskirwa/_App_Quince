466c391a02fdaf5bcc3f73ed5b6ba532b7220f3f
Annaliskirwa <annaliskirwa@gmail.com>
2022-02-12 23:39:44 +0300

    Added the homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { useState, useEffect } from "react";
-import { Link, BrowserRouter as Router, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-const HomePage = () => {
-  return <>Home Page</>;
-};
-
-const App = () => {
+function App() {
   return (
-    <>
-      <Router>
-        <Route exact path="/" component={HomePage} />
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
