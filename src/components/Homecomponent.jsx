568f40eb6d93c8aecd34a4eee957a7b95a26eb9e
Annaliskirwa <annaliskirwa@gmail.com>
2022-02-13 22:23:51 +0300

    Using axios to consume the api

--- src/components/Homecomponent.jsx
+++ src/components/Homecomponent.jsx
@@ -3,16 +3,28 @@ import React, {useState, useEffect} from "react";
 const Homepage = ()=>{
     const [isLoading, setIsLoading] = useState(true);
     const [data, setData] = useState();
+    const axios = require('axios');
 
     useEffect(()=>{
-        fetch("https://swapi.dev/api/people/",{})
-        .then((res)=>res.json())
-        .then((response)=>{
-            setData(response.results);
-            console.log(response.results)
-            setIsLoading(false);
-        })
-        .catch((error)=>console.log(error));
+        const fetchActors = async()=>{
+            let res = await axios({
+                method: "GET",
+                url: "https://swapi.dev/api/people/"
+            });
+            var response = res.data.results
+            setData(response)
+            console.log(response)
+
+        }
+        fetchActors()
+        // fetch("https://swapi.dev/api/people/",{})
+        // .then((res)=>res.json())
+        // .then((response)=>{
+        //     setData(response.results);
+        //     console.log(response.results)
+        //     setIsLoading(false);
+        // })
+        // .catch((error)=>console.log(error));
     },[]);
     
     return (
