6b26ec674dcb6be21be503f1f5b616266ba389c2
Annaliskirwa <annaliskirwa@gmail.com>
2022-02-17 11:32:59 +0300

    Added the line chart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import BarChart from './Charts/BarChart';
 import DoughnutChart from './Charts/DoughnutChart';
+import LineChart from './Charts/LineChart';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Chart js and restful apis</h1>
       <BarChart/>
       <DoughnutChart/>
+      <LineChart/>
     </div>
   );
 }
