e9d83fda18ea5c8fc1f4468e1f9b51721261e3f2
Annaliskirwa <annaliskirwa@gmail.com>
2022-02-17 11:28:40 +0300

    Added the doughnut chart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import BarChart from './Charts/BarChart';
+import DoughnutChart from './Charts/DoughnutChart';
 
 function App() {
   return (
     <div className="App">
       <h1>Chart js and restful apis</h1>
       <BarChart/>
+      <DoughnutChart/>
     </div>
   );
 }
