31d7fb150e0d7ddf731b80a9560c306666973935
Annaliskirwa <annaliskirwa@gmail.com>
2022-02-17 11:40:58 +0300

    Added the pie chart

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import BarChart from './Charts/BarChart';
 import DoughnutChart from './Charts/DoughnutChart';
 import LineChart from './Charts/LineChart';
+import PieChart from './Charts/PieChart';
 
 function App() {
   return (
     <div className="App">
       <h1>Chart js and restful apis</h1>
+      <h2>Bar Chart</h2>
       <BarChart/>
+      <h2>Doughnut Chart</h2>
       <DoughnutChart/>
+      <h2>Line Chart</h2>
       <LineChart/>
+      <h2>Pie Chart</h2>
+      <PieChart/>
     </div>
   );
 }
